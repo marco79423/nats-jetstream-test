4bf5c7198c7fea390141741c39faef893e0a1e76
Marco Li <marco_li@paradise-soft.com.tw>
2021-09-22 18:16:20 +0800

    調整清空訊息的寫法

--- tester/jetstream_purge_stream_tester/main.go
+++ tester/jetstream_purge_stream_tester/main.go
@@ -34,6 +34,23 @@ func TestJetStreamPurgeStream() error {
 		return xerrors.Errorf("取得 JetStream 的 Context 失敗: %w", err)
 	}
 
+	// JetStream 需要顯示管理 Stream
+	streamName := conf.Testers.JetStreamPurgeStreamTest.Stream
+
+	// 可透過 StreamInfo 取得 Stream 相關的資訊
+	if stream, _ := js.StreamInfo(streamName); stream == nil {
+
+		// 如果不存在需要主動建立 Stream
+		if _, err = js.AddStream(&nats.StreamConfig{
+			Name: streamName,
+			Subjects: []string{
+				conf.Testers.JetStreamPurgeStreamTest.Subject, // 例子是用 ray.fuck，但也可以設定 ray.*
+			},
+		}); err != nil {
+			return xerrors.Errorf("建立 Stream %s 失敗: %w", streamName, err)
+		}
+	}
+	
 	for _, count := range conf.Testers.JetStreamPurgeStreamTest.Counts {
 		if err := TestPurgeStream(conf, js, count); err != nil {
 			return xerrors.Errorf("測試 Purge Stream 失敗: %w", err)
@@ -46,29 +63,14 @@ func TestJetStreamPurgeStream() error {
 func TestPurgeStream(conf *config.Config, js nats.JetStreamContext, count int) error {
 	fmt.Printf("\n開始測試 JetStream 的 Purge Stream (%d 筆) 的效能\n", count)
 
-	// JetStream 需要顯示管理 Stream
 	streamName := conf.Testers.JetStreamPurgeStreamTest.Stream
 
-	// 可透過 StreamInfo 取得 Stream 相關的資訊
-	stream, _ := js.StreamInfo(streamName)
-
-	// 先把之前的 Stream 刪掉 (正常不用這麼做，這是為了測試用)
-	if stream != nil {
-		if err := js.DeleteStream(streamName); err != nil {
-			return xerrors.Errorf("刪除 Stream %s 失敗: %w", streamName, err)
-		}
+	// 清空之間的資料
+	if err := js.PurgeStream(streamName); err != nil {
+		return xerrors.Errorf("Purge Stream 失敗: %w", err)
 	}
 
-	// 重新建立一個 Stream
-	if _, err := js.AddStream(&nats.StreamConfig{
-		Name: streamName,
-		Subjects: []string{
-			conf.Testers.JetStreamPurgeStreamTest.Subject, // 例子是用 ray.fuck，但也可以設定 ray.*
-		},
-	}); err != nil {
-		return xerrors.Errorf("建立 Stream %s 失敗: %w", streamName, err)
-	}
-
+	// 發布足夠的訊息
 	for i := 0; i < count; i++ {
 		_, err := js.Publish(conf.Testers.JetStreamPurgeStreamTest.Subject, []byte(fmt.Sprintf("%d", i)))
 		if err != nil {
@@ -77,6 +79,7 @@ func TestPurgeStream(conf *config.Config, js nats.JetStreamContext, count int) e
 		// fmt.Println(i)
 	}
 
+	// 清空資訊
 	now := time.Now()
 	if err := js.PurgeStream(streamName); err != nil {
 		return xerrors.Errorf("Purge Stream 失敗: %w", err)
